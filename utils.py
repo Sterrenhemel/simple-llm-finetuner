204a9d7c4b6e4bbd57cf737f74c3afdc07178566
pengdaqian <pengdaqian.0@bytedance.com>
2023-05-10 16:50:19 +0800

    add more model

--- utils.py
+++ utils.py
@@ -3,6 +3,14 @@ import re
 from pathlib import Path
 
 
-def get_datasets(path: str, ext: str):
-    return ['None'] + sorted(set([k.stem for k in Path(path).glob(f'*.{ext}') if k.stem != 'put-trainer-datasets-here']), key=natural_keys)
+def atoi(text):
+    return int(text) if text.isdigit() else text.lower()
 
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
+
+
+def get_datasets(path: str, ext: str):
+    return ['None'] + sorted(
+        set([k.stem for k in Path(path).glob(f'*.{ext}') if k.stem != 'put-trainer-datasets-here']), key=natural_keys)
